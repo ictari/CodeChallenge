89be745d73e754599aad0a8ca2e93e15cf802412
Nick Bates <contact@nickbates.com>
2021-03-13 22:23:47 +0100

    Alternative WebApi Solution with Docker support

---
+++ Challenge/Challenge.Api/Controllers/ChallengeController.cs
@@ -0,0 +1,21 @@
+﻿using Challenge.Lib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Challenge.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChallengeController : ControllerBase
+    {
+        private readonly ILogger<ChallengeController> _logger;
+
+        public ChallengeController(ILogger<ChallengeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{value}")]
+        public IActionResult Get([FromRoute] int value) => new JsonResult(new string[] { "result", new Service().Get(value) });
+    }
+}
